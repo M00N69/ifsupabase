2d59ac001f199bcfedbf5f0b98ef5ef1d6992293
MOON <133333114+M00N69@users.noreply.github.com>
2024-12-08 16:39:50 +0100

    Create ifsactionplan.py

---
+++ ifsactionplan.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from pages.upload import render_upload_page
+from pages.nonconformities import render_nonconformities_page
+
+def main():
+    """Main application logic."""
+    pages = {
+        "Téléverser un fichier Excel": render_upload_page,
+        "Visualiser les Non-Conformités": render_nonconformities_page,
+    }
+    st.sidebar.title("Navigation")
+    page = st.sidebar.selectbox("Choisissez une page", list(pages.keys()))
+    pages[page]()  # Call the selected page
+
+if __name__ == "__main__":
+    main()
